8322019250932218420a4c0fc67715d79bf55fc2
Christian <cescobar852@gmail.com>
2026-05-28 21:31:04 -0500

    Initial project setup

---
+++ src/main/java/com/arcade/MainApplication.java
@@ -0,0 +1,32 @@
+package com.arcade;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Main entry point for the arcade educational game.
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label titleLabel = new Label("Arcade Educational Game");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(titleLabel);
+
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("Arcade Educational Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
